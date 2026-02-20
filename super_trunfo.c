ecca3872abc9f137c9036448614a7fbaef1a415b
estudoprogramacao1983 <wagnerteixeirap@hotmail.com>
2026-02-20 23:21:10 +0000

    inclusao de variaveis

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    printf("Desafio Super Trunfo em C");
+    printf("Desafio Super Trunfo em C\n");
     
     char estado;
     char codigo [3];
@@ -11,20 +11,27 @@ int main(){
     float pib;
     int pturistico;//quantidade de pontos turisticos.
 
+    printf("Digite a sua carta: ");
+    scanf("%c", &estado);
 
+    printf("Digite o Código da Cidade: ");
+    scanf("%s", &codigo);
 
+    printf("Digite o Nome da Cidade: ");
+    scanf("%s", &cidade);
 
+    printf("Digite a População da Cidade: ");
+    scanf("%d", &populacao);
 
+    printf("Digite a Área em KM² da Cidade: ");
+    scanf("%f", &area);
 
+    printf("Digite o PIB em Bilões de Reais da Cidade: ");
+    scanf("%f", &pib);
 
+    printf("Digite a Quantidade de Pontos Turísticos da Cidade; ");
+    scanf("%d", pturistico);
 
-
-
-
-
-
-
-
-
-
+    
+    return 0;
 }

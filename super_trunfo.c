7c3850a72128065afa6489965073205f8857cbd3
estudoprogramacao1983 <wagnerteixeirap@hotmail.com>
2026-02-20 22:51:00 +0000

    Criei printf

---
+++ super_trunfo.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+    printf("Desafio Super Trunfo em C");
+    
+    char estado;
+    char codigo [3];
+    char cidade [20];
+    int populacao;
+    float area;
+    float pib;
+    int pturistico;//quantidade de pontos turisticos.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

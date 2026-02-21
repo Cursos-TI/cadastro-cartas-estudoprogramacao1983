95816901bd43d8b4767629403126de0aa08129a0
estudoprogramacao1983 <wagnerteixeirap@hotmail.com>
2026-02-21 12:55:09 +0000

    ENTRADA DE DADOS DAS CARTAS

--- super_trunfo.c
+++ super_trunfo.c
@@ -2,35 +2,103 @@
 
 int main(){
     printf("Desafio Super Trunfo em C\n");
+
+    /* ====================
+    DECLARAÇÃO DE VARIÁVEIS
+    =======================*/
+
+    //CARTA 1
+    char estado1;
+    char codigo1 [4];
+    char nomecidade1 [50];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontosturisticos1;
+
+    //CARTA 2
+    char estado2;
+    char codigo2;
+    char nomecidade2;
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontosturisticos2;
+
+    /*=========================
+    ENTRADA DE DADOS DA CARTA 1
+    ===========================*/
+
+    printf("==== Cadastro da Carta 1 ====\n");
+
+    printf("Estado A: ");
+    scanf("%c", &estado1);
+
+    printf("Código da carta: A01");
+    scanf("%s", &codigo1);
+
+    printf("Nome da Cidade: Fortaleza");
+    scanf("%s", &nomecidade1);
+
+    printf("População: 5000000000");
+    scanf("%d", &populacao1);
+
+    printf("Área (em KM²); 1250.23");
+    scanf("%f", &area1);
+
+    printf("PIB (em bilões de reais): 400.29");
+    scanf("%f"), &pib1);
+
+    printf("Número de Pontos Turísticos: 40");
+    scanf("%d", &pontosturisticos1);
+
+    /*=========================
+    ENTRADA DE DADOS DA CARTA 2
+    ===========================*/
+
+    printf("==== Cadastro da Carta 2 ====\n");
+
+    printf("Estado B: ");
+    scanf("%c", &estado2);
+
+    printf("Código da carta: B01");
+    scanf("%s", &codigo2);
+
+    printf("Nome da Cidade: Curitiba");
+    scanf("%s", &nomecidade2);
+
+    printf("População: 5500000000");
+    scanf("%d", &populacao2);
+
+    printf("Área (em KM²); 1050.23");
+    scanf("%f", &area2);
+
+    printf("PIB (em bilões de reais): 430.10");
+    scanf("%f"), &pib2);
+
+    printf("Número de Pontos Turísticos: 35");
+    scanf("%d", &pontosturisticos2);
+
+    /*=========================
+        EXIBIÇÃO DOS DADOS
+    ===========================*/
+
     
-    char estado;
-    char codigo [3];
-    char cidade [20];
-    int populacao;
-    float area;
-    float pib;
-    int pturistico;//quantidade de pontos turisticos.
 
-    printf("Digite a sua carta: ");
-    scanf("%c", &estado);
 
-    printf("Digite o Código da Cidade: ");
-    scanf("%s", &codigo);
 
-    printf("Digite o Nome da Cidade: ");
-    scanf("%s", &cidade);
 
-    printf("Digite a População da Cidade: ");
-    scanf("%d", &populacao);
 
-    printf("Digite a Área em KM² da Cidade: ");
-    scanf("%f", &area);
 
-    printf("Digite o PIB em Bilões de Reais da Cidade: ");
-    scanf("%f", &pib);
 
-    printf("Digite a Quantidade de Pontos Turísticos da Cidade; ");
-    scanf("%d", pturistico);
+
+
+
+
+
+
+
+    
 
     
     return 0;
